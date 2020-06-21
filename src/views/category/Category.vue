811f86d1631860330403095a246cde74799bdb74
OeilShine <oeilshine@gmail.com>
2020-06-21 12:10:37 +0800

    first commit

---
+++ src/views/category/Category.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>This is a Category page.</h2>
+</template>
+
+<script>
+export default {
+  name: "Category"
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2> This is a Home page.</h2>
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>
+
+<style>
+
+</style>
